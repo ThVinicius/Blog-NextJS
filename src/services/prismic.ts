14ccda16e3786e6adbd6b3e0a1897278f1eb1e62
Vinicius Pacheco dos Santos <vinicioss9955@gmail.com>
2023-04-11 12:47:17 -0300

    feat: public env

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,7 +1,7 @@
 import Prismic from '@prismicio/client'
 
 export function getPrismicClient(req?: unknown) {
-  const prismic = Prismic.client(process.env.PRISMIC_API_URL, {
+  const prismic = Prismic.client(process.env.NEXT_PUBLIC_PRISMIC_API_URL, {
     req
   })
 
